9957bcf70bcb38ca7286bc0f13c7b42a49164ce7
Yu <peng.yu@intel.com>
2020-11-03 19:56:58 +0800

    redis utils, get block locations from redis

--- oap-fs/pmem-cached-hcfs/src/main/java/com/intel/oap/fs/hadoop/cachedfs/PMemBlockLocationStore.java
+++ oap-fs/pmem-cached-hcfs/src/main/java/com/intel/oap/fs/hadoop/cachedfs/PMemBlockLocationStore.java
@@ -3,7 +3,7 @@ package com.intel.oap.fs.hadoop.cachedfs;
 public interface PMemBlockLocationStore {
     void addBlockLocation(PMemBlock block, String host);
 
-    void setBlockLocations(PMemBlock block, String[] hosts);
-
     PMemBlockLocation getBlockLocation(PMemBlock block);
+
+    PMemBlockLocation[] getBlockLocations(PMemBlock[] blocks, boolean consecutive);
 }
