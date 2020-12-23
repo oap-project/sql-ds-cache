40938d01e59bb8a85285ce40d916bf7795a3196e
Ji, Kunshang <kunshang.ji@intel.com>
2020-12-23 00:29:24 -0800

    bug fix: 1) cpp readBatch miss add totalRowsRead 2) java add DateType, TimeStampType support 3) java fix get string method 4) java fix isNullAt

---
+++ oap-ape/ape-java/ape-spark/src/main/java/org/apache/spark/sql/execution/datasources/parquet/ParquetNativeRecordReaderWrapper.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.datasources.parquet;
+
+import com.intel.ape.ParquetReaderJNI;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.parquet.hadoop.ParquetInputSplit;
+import org.apache.spark.sql.execution.vectorized.NativeColumnVector;
+import org.apache.spark.sql.types.*;
+import org.apache.spark.sql.vectorized.ColumnarBatch;
+import org.apache.spark.unsafe.Platform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class ParquetNativeRecordReaderWrapper extends RecordReader<Void, Object> {
+  private static final Logger LOG = LoggerFactory.getLogger(ParquetNativeRecordReaderWrapper.class);
+
+  long reader = 0;
+  int batchSize = 0;
+
+  long bufferPtrs[];
+  long nullPtrs[];
+
+  private ColumnarBatch columnarBatch;
+
+  private NativeColumnVector[] columnVectors;
+
+  StructType sparkSchema;
+
+  public ParquetNativeRecordReaderWrapper(int capacity) {
+    this.batchSize = capacity;
+  }
+
+  @Override
+  public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+    // todo: get hdfs and schema info
+    Configuration configuration = taskAttemptContext.getConfiguration();
+    String reqSchema = configuration.get(ParquetReadSupport$.MODULE$.SPARK_ROW_REQUESTED_SCHEMA());
+    sparkSchema = StructType$.MODULE$.fromString(reqSchema);
+    ParquetInputSplit split = (ParquetInputSplit) inputSplit;
+    String fileName = split.getPath().toUri().getRawPath();
+    String hdfs = configuration.get("fs.defaultFS"); // this string is like hdfs://host:port
+    String[] res = hdfs.split(":");
+    String hdfsHost = res[1].substring(2);
+    int hdfsPort = Integer.parseInt(res[2]);
+    LOG.info("filename is " + fileName + " hdfs is " + hdfsHost + " " + hdfsPort);
+    LOG.info("schema is " + sparkSchema.json());
+    reader = ParquetReaderJNI.init(fileName, hdfsHost, hdfsPort, sparkSchema.json());
+  }
+
+  @Override
+  public boolean nextKeyValue() throws IOException, InterruptedException {
+    if (columnarBatch == null) initBatch();
+    return nextBatch();
+  }
+
+  @Override
+  public Void getCurrentKey() throws IOException, InterruptedException {
+    return null;
+  }
+
+  @Override
+  public Object getCurrentValue() throws IOException, InterruptedException {
+    return columnarBatch;
+  }
+
+  @Override
+  public float getProgress() throws IOException, InterruptedException {
+    // todo: impl getTotalRows method in JNI layer
+    return 0;
+  }
+
+  @Override
+  public void close() throws IOException {
+    // free buffers
+
+    // close columnBatch
+    if (columnarBatch != null) {
+      columnarBatch.close();
+      columnarBatch = null;
+    }
+
+    // close reader
+    if (reader != 0) {
+      ParquetReaderJNI.close(reader);
+    }
+  }
+
+  // todo: impl
+  private boolean nextBatch() {
+    if (reader == 0 ) {
+      return false;
+    }
+    int rowsRead = ParquetReaderJNI.readBatch(reader, batchSize, bufferPtrs, nullPtrs);
+    if (rowsRead <= 0) {
+      return false;
+    }
+    // build columnVectors by buffers and nulls
+    columnarBatch.setNumRows(0);
+
+    columnarBatch.setNumRows(rowsRead);
+    return true;
+  }
+
+  // todo: impl
+  private void initBatch() {
+    // allocate buffers here according schema
+    StructField[] fileds = sparkSchema.fields();
+    columnVectors = new NativeColumnVector[fileds.length];
+    bufferPtrs = new long[fileds.length];
+    nullPtrs = new long[fileds.length];
+    for (int i = 0; i < fileds.length; i++) {
+      DataType type = fileds[i].dataType();
+      columnVectors[i] = new NativeColumnVector(type);
+
+      // TODO: in fact we don't need a batchSize buffer to store nulls since it uses bitmap
+      long nullPtr = Platform.allocateMemory(batchSize);
+      long bufferPtr = 0;
+      if (type instanceof BooleanType || type instanceof IntegerType || type instanceof LongType
+              || type instanceof FloatType || type instanceof DoubleType
+              || type instanceof DateType || type instanceof TimestampType) {
+        bufferPtr = Platform.allocateMemory(batchSize * type.defaultSize());
+      } else if (type instanceof BinaryType || type instanceof StringType) {
+        bufferPtr = Platform.allocateMemory(batchSize * 16);
+      } else if (DecimalType.is32BitDecimalType(type)) {
+        bufferPtr = Platform.allocateMemory(batchSize * 4);
+      } else if (DecimalType.is64BitDecimalType(type)) {
+        bufferPtr = Platform.allocateMemory(batchSize * 8);
+      } else {
+        // TODO: will add byte and short type. Not sure about Map
+        throw new UnsupportedOperationException("Type not support yet");
+      }
+      columnVectors[i].set(bufferPtr, nullPtr, batchSize);
+      bufferPtrs[i] = bufferPtr;
+      nullPtrs[i] = nullPtr;
+    }
+
+    columnarBatch = new ColumnarBatch(columnVectors);
+  }
+}
