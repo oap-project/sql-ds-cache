38e59cfb8028d47e4c136f9365066c6bd07d15ac
Han, Xin <xin.han@intel.com>
2021-02-03 20:26:22 -0800

    [POAE7-809]Inspect abnormal statistics of benchmark queries with native reader

--- oap-ape/ape-java/ape-flink/src/main/java/org/apache/flink/formats/parquet/utils/ParquetNativeRecordReaderWrapper.java
+++ oap-ape/ape-java/ape-flink/src/main/java/org/apache/flink/formats/parquet/utils/ParquetNativeRecordReaderWrapper.java
@@ -18,10 +18,15 @@
 package org.apache.flink.formats.parquet.utils;
 
 import static org.apache.parquet.Preconditions.checkArgument;
-import static org.apache.parquet.hadoop.ParquetFileReader.readFooter;
+import static org.apache.parquet.hadoop.ParquetFileWriter.MAGIC;
+import static org.apache.parquet.bytes.BytesUtils.readIntLittleEndian;
+import static org.apache.parquet.format.Util.readFileMetaData;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.intel.ape.ParquetReaderJNI;
@@ -42,11 +47,17 @@ import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.RowType;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.parquet.filter2.predicate.FilterPredicate;
-import org.apache.parquet.hadoop.metadata.BlockMetaData;
-import org.apache.parquet.hadoop.metadata.ParquetMetadata;
+import org.apache.parquet.format.ColumnChunk;
+import org.apache.parquet.format.ColumnMetaData;
+import org.apache.parquet.format.FileMetaData;
+import org.apache.parquet.format.RowGroup;
+import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.apache.parquet.io.SeekableInputStream;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.OriginalType;
 import org.apache.parquet.schema.PrimitiveType;
+import org.apache.parquet.bytes.ByteBufferInputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -86,8 +97,8 @@ public class ParquetNativeRecordReaderWrapper {
         }
 
         ConvertToJson message = new ConvertToJson(fieldTypeList, projectedFields);
-        reader = ParquetReaderJNI.init(
-                fileName, hdfsHost, hdfsPort, message.toJson(), inputSplitRowGroupStartIndex, inputSplitRowGroupNum);
+        reader = ParquetReaderJNI.init(fileName, hdfsHost, hdfsPort, message.toJson(), inputSplitRowGroupStartIndex,
+                inputSplitRowGroupNum);
 
         LOG.info("native parquet reader initialized");
 
@@ -98,31 +109,79 @@ public class ParquetNativeRecordReaderWrapper {
     public void getRequiredSplitRowGroup(FileSourceSplit split, Configuration configuration) throws IOException {
         long splitStart = split.offset();
         long splitSize = split.length();
-        org.apache.hadoop.fs.Path file = new org.apache.hadoop.fs.Path(split.path().toUri());
-
-        ParquetMetadata footer = readFooter(configuration, file);
-        List<BlockMetaData> blocks = footer.getBlocks();
-
-        long currentOffset = 0L;
-        long PARQUET_MAGIC_NUMBER = 4L;
-        currentOffset += PARQUET_MAGIC_NUMBER;
-
-        int rowGroupIndex = 0;
-
-        boolean flag = false;
-        for (BlockMetaData blockMetaData : blocks) {
-            if (splitStart <= currentOffset && splitStart + splitSize >= currentOffset) {
-                if (!flag) {
-                    flag = true;
-                    inputSplitRowGroupStartIndex = rowGroupIndex;
-                }
-                inputSplitRowGroupNum ++;
-            }
-            rowGroupIndex ++;
-            currentOffset += blockMetaData.getCompressedSize();
-        }
+        InputStream footerBytesStream = getFooterBytesStream(split, configuration);
+        filterFileMetaDataByMidpoint(readFileMetaData(footerBytesStream), splitStart, splitStart + splitSize);
     }
 
+    private InputStream getFooterBytesStream(FileSourceSplit split, Configuration configuration) throws IOException {
+
+        org.apache.hadoop.fs.Path file = new org.apache.hadoop.fs.Path(split.path().toUri());
+        HadoopInputFile inputFile = HadoopInputFile.fromPath(file, configuration);
+
+        SeekableInputStream f = inputFile.newStream();
+        long fileLen = inputFile.getLength();
+
+        int FOOTER_LENGTH_SIZE = 4;
+        if (fileLen < MAGIC.length + FOOTER_LENGTH_SIZE + MAGIC.length) { // MAGIC + data + footer + footerIndex + MAGIC
+            throw new RuntimeException(file.toString() + " is not a Parquet file (too small length: " + fileLen + ")");
+        }
+
+        long footerLengthIndex = fileLen - FOOTER_LENGTH_SIZE - MAGIC.length;
+
+        f.seek(footerLengthIndex);
+        int footerLength = readIntLittleEndian(f);
+        byte[] magic = new byte[MAGIC.length];
+        f.readFully(magic);
+        if (!Arrays.equals(MAGIC, magic)) {
+            throw new RuntimeException(file.toString() + " is not a Parquet file. expected magic number at tail "
+                    + Arrays.toString(MAGIC) + " but found " + Arrays.toString(magic));
+        }
+        long footerIndex = footerLengthIndex - footerLength;
+        LOG.debug("read footer length: {}, footer index: {}", footerLength, footerIndex);
+        if (footerIndex < MAGIC.length || footerIndex >= footerLengthIndex) {
+            throw new RuntimeException("corrupted file: the footer index is not within the file: " + footerIndex);
+        }
+        f.seek(footerIndex);
+        ByteBuffer footerBytesBuffer = ByteBuffer.allocate(footerLength);
+        f.readFully(footerBytesBuffer);
+        LOG.debug("Finished to read all footer bytes.");
+        footerBytesBuffer.flip();
+        InputStream footerBytesStream = ByteBufferInputStream.wrap(footerBytesBuffer);
+        return footerBytesStream;
+    }
+
+    private void filterFileMetaDataByMidpoint(FileMetaData metaData, long startOffset, long endOffset) {
+        List<RowGroup> rowGroups = metaData.getRow_groups();
+        int inputIndex = 0;
+        Boolean flag = false;
+        for (RowGroup rowGroup : rowGroups) {
+            long totalSize = 0;
+            long startIndex = getOffset(rowGroup.getColumns().get(0));
+            for (ColumnChunk col : rowGroup.getColumns()) {
+                totalSize += col.getMeta_data().getTotal_compressed_size();
+            }
+            long midPoint = startIndex + totalSize / 2;
+
+            if (midPoint >= startOffset && midPoint < endOffset) {
+                if (!flag) {
+                    inputSplitRowGroupStartIndex = inputIndex;
+                    flag = true;
+                }
+                inputSplitRowGroupNum += 1;
+            }
+            inputIndex++;
+        }
+        LOG.info("inputIndex: {}, finputSplitRowGroupNum: {}", inputIndex, inputSplitRowGroupNum);
+    }
+
+    protected long getOffset(ColumnChunk columnChunk) {
+        ColumnMetaData md = columnChunk.getMeta_data();
+        long offset = md.getData_page_offset();
+        if (md.isSetDictionary_page_offset() && offset > md.getDictionary_page_offset()) {
+            offset = md.getDictionary_page_offset();
+        }
+        return offset;
+    }
 
     public long getReader() {
         return this.reader;
