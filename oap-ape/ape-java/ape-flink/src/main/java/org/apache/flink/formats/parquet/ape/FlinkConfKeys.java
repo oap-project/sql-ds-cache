fce0bcf12fd0a518fc5b1edda7fb215d57a58a18
Yu, Peng <peng.yu@intel.com>
2021-02-08 21:36:10 -0800

    [POAE7-843] add setAggStr in native reader wrapper

--- oap-ape/ape-java/ape-flink/src/main/java/org/apache/flink/formats/parquet/ape/FlinkConfKeys.java
+++
@@ -1,6 +0,0 @@
-package org.apache.flink.formats.parquet.ape;
-
-public class FlinkConfKeys {
-    // use native parquet reader or not.
-    public static final String USE_NATIVE_PARQUET_READER = "table.exec.hive.parquet-native-reader";
-}
