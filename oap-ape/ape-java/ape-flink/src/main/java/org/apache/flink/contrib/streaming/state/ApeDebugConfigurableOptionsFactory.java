a2d7c3e04510ce500ae10c5e4d98afde9de9ad5c
Yu, Peng <peng.yu@intel.com>
2021-08-24 17:44:50 +0800

    fix code style

--- oap-ape/ape-java/ape-flink/src/main/java/org/apache/flink/contrib/streaming/state/ApeDebugConfigurableOptionsFactory.java
+++ oap-ape/ape-java/ape-flink/src/main/java/org/apache/flink/contrib/streaming/state/ApeDebugConfigurableOptionsFactory.java
@@ -1,20 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.apache.flink.contrib.streaming.state;
 
@@ -22,15 +22,19 @@ import org.rocksdb.DBOptions;
 
 import java.util.Collection;
 
+/**
+* A option factory for RocksDB in Flink.
+* This factory set direct IO options when RocksDB reads / writes data.
+*/
 public class ApeDebugConfigurableOptionsFactory extends DefaultConfigurableOptionsFactory {
-    @Override
-    public DBOptions createDBOptions(DBOptions currentOptions, Collection<AutoCloseable> handlesToClose) {
-        DBOptions options = super.createDBOptions(currentOptions, handlesToClose);
+	@Override
+	public DBOptions createDBOptions(DBOptions currentOptions, Collection<AutoCloseable> handlesToClose) {
+		DBOptions options = super.createDBOptions(currentOptions, handlesToClose);
 
-        // use Direct IO for performance profiling
-        options.setUseDirectReads(true);
-        options.setUseDirectIoForFlushAndCompaction(true);
+		// use Direct IO for performance profiling
+		options.setUseDirectReads(true);
+		options.setUseDirectIoForFlushAndCompaction(true);
 
-        return options;
-    }
+		return options;
+	}
 }
