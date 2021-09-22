3a72c3b832024b623383dfca41a53ae519bd89c1
Yu, Peng <peng.yu@intel.com>
2021-09-22 10:04:38 +0800

    fix code style

--- oap-ape/ape-java/ape-flink/ape-flink-common/src/main/java/org/apache/flink/table/connector/source/abilities/SupportsAggregationPushDown.java
+++ oap-ape/ape-java/ape-flink/ape-flink-common/src/main/java/org/apache/flink/table/connector/source/abilities/SupportsAggregationPushDown.java
@@ -28,10 +28,10 @@ import java.util.List;
  * data source. Commonly, filters should be pushed down too so that the data source can get the
  * partial aggregation result.
  *
- * <p>
- * For example,
- *     select sum(d_date_sk) from date_dim where d_year = 2000;
- * The original plan of above sql in Flink may be:
+ * <p>For example, select sum(d_date_sk) from date_dim where d_year = 2000; The original plan of
+ * above sql in Flink may be:
+ *
+ * <pre>@{code
  * == Optimized Logical Plan ==
  * HashAggregate(isMerge=[true], select=[Final_SUM(sum$0) AS EXPR$0])
  * +- Exchange(distribution=[single])
@@ -40,24 +40,31 @@ import java.util.List;
  *          +- TableSourceScan(table=[[myhive, tpcds_1g_snappy, date_dim, filter=[],
  *              project=[d_date_sk, d_year]]], fields=[d_date_sk, d_year])
  *
- * The plan after filters are pushed down:
- *== Optimized Logical Plan ==
+ * }</pre>
+ *
+ * <p>The plan after filters are pushed down:
+ *
+ * <pre>@{code
+ * == Optimized Logical Plan ==
  * HashAggregate(isMerge=[true], select=[Final_SUM(sum$0) AS EXPR$0])
  * +- Exchange(distribution=[single])
  *    +- LocalHashAggregate(select=[Partial_SUM(d_date_sk) AS sum$0])
  *       +- TableSourceScan(table=[[myhive, tpcds_1g_snappy, date_dim,
  *          filter=[equals(d_year, 2000)], project=[d_date_sk]]], fields=[d_date_sk])
- * </p>
  *
- * <p>
- * Moreover, aggregations are pushed down, the optimized plan becomes:
+ * }</pre>
+ *
+ * <p>Moreover, aggregations are pushed down, the optimized plan becomes:
+ *
+ * <pre>@{code
  * == Optimized Logical Plan ==
  * HashAggregate(isMerge=[true], select=[Final_SUM(sum$0) AS EXPR$0])
  * +- Exchange(distribution=[single])
  *    +- TableSourceScan(table=[[myhive, tpcds_1g_snappy, date_dim,
  *          filter=[equals(d_year, 2000)], project=[d_date_sk], aggregation=[sum$0]]],
  *          fields=[sum$0])
- * </p>
+ *
+ * }</pre>
  */
 public interface SupportsAggregationPushDown {
 
